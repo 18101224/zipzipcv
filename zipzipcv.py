c5cba79d5bd1b2e6a5064272ad464435e82bdc22
18101224 <91134375+18101224@users.noreply.github.com>
2023-03-16 09:17:58 +0900

    Add files via upload

--- zipzipcv.py
+++ zipzipcv.py
@@ -1,21 +1,27 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
 import numpy as np
 import cv2 as cv
 img_file = './sunny.jpeg'
-img = cv.imread(img_file)
+img = cv.imread(img_file)/255
 #for display the original picture
 import matplotlib.pyplot as plt
 plt.title('The original image')
 plt.imshow(img)
 plt.show()
 from sklearn.cluster import KMeans
-img_flatten = img / 255
-img_flatten = img_flatten.reshape(-1,3)
+img_flatten = img.reshape(-1,3)
 img_flatten.shape
 zipper = KMeans(n_clusters=4)
 zipper.fit(img_flatten)
 zipped_pixel_label = zipper.predict(img_flatten)
 zipped_img = zipper.cluster_centers_[zipped_pixel_label].reshape(img.shape)
 plt.imshow(zipped_img)
-cv.imshow('before and after',np.hstack(img,zipped_img))
+cv.imshow('before and after',np.hstack((img,zipped_img)))
 cv.waitKey()
 cv.destroyWindow()
